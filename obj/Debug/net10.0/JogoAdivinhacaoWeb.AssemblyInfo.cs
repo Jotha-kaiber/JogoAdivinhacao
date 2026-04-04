fb6a5b28a3ba3529377173910361c28ec08ae6af
Jotha <seu-email@exemplo.com>
2026-04-03 21:49:34 -0300

    Arquivos de publish reais adicionados

--- obj/Debug/net10.0/JogoAdivinhacaoWeb.AssemblyInfo.cs
+++ obj/Debug/net10.0/JogoAdivinhacaoWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JogoAdivinhacaoWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa40aa9125acbb3e4c442fec74ed7c673a99cc23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e20cba0acaad0c2629e12cac69fae2725b1ab681")]
 [assembly: System.Reflection.AssemblyProductAttribute("JogoAdivinhacaoWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JogoAdivinhacaoWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
